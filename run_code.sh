18b4b731e39a7abf67e927c2c2ef93d5cf18d308
goelsahab0170 <159719809+goelsahab0170@users.noreply.github.com>
2025-05-15 07:17:06 +0530

    Create run_code.sh

---
+++ run_code.sh
@@ -0,0 +1,7 @@
+To run the code in linux terminal, you need to install some important libraries:
+Python 3.6 or later is recommended.
+Use these commands to install modules and libraries:
+sudo apt install python3-tk
+pip install psutil
+pip install fpdf
+sudo apt install apache2
